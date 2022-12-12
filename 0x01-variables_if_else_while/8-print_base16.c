d9be93650404d27a271ee8bd76ac6d62d0281dff
Nyokayo <knyokayo@gmail.com>
2022-12-12 08:34:49 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,13 +4,13 @@
  * main - prints all the numbers of the base 16 in lowercase
  * Return:0
  */
-int main (void)
+int main(void)
 {
 	int num;
 	char la;
 
 	for (num = 0; num < 10; num++)
-		putchar((num %10) + '0');
+		putchar((num % 10) + '0');
 
 	for (la = 'a'; la <= 'f'; la++)
 		putchar(la);
