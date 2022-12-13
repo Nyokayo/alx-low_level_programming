52f5c9e3bbc3ad2b409fcc247c8ae73a66b4a9ff
Nyokayo <knyokayo@gmail.com>
2022-12-13 14:42:04 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet - print the alphabet in lowercase
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -11,3 +12,4 @@ void print_alphabet(void)
 		_putchar(la);
 
 	_putchar('\n');
+}
