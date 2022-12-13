cabab435981ba591ecae7386e5a68679d3519650
Nyokayo <knyokayo@gmail.com>
2022-12-13 14:38:42 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'a'; la++)
+		_putchar(la);
+
+	_putchar('\n');
